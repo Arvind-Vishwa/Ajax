59953043be31fbf249b1738f1acc4bdcff64af94
Arvind-Vishwa <av0216380@gmail.com>
2025-01-29 18:13:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('fetchDataBtn').addEventListener('click', fetchData);
+});
+
+function fetchData() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts', true);
+
+    xhr.onload = function() {
+        if (this.status === 200) {
+            const posts = JSON.parse(this.responseText);
+            let output = '';
+
+            posts.forEach(function(post) {
+                output += `
+                    <div id="cardBody" class="card mb-5">
+                        <div class="card-body">
+                            <h5 class="card-title">${post.title}</h5>
+                            <p class="card-text">${post.body}</p>
+                        </div>
+                    </div>
+                `;
+            });
+
+            document.getElementById('dataContainer').innerHTML = output;
+        } else {
+            document.getElementById('dataContainer').innerHTML = '<p class="text-danger">Error fetching data</p>';
+        }
+    };
+
+    xhr.onerror = function() {
+        document.getElementById('dataContainer').innerHTML = '<p class="text-danger">Request error...</p>';
+    };
+
+    xhr.send();
+}
